a876d6ac5dae243f50ca28b6060c09a181f0210f
EzequielGnich <ezequielgnich@live.com>
2019-08-11 20:34:00 -0300

    Importado socket.io e criado a conexão com o frontend e mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,18 @@
 const cors = require('cors')
 const mongoose = require('mongoose')
 const express = require('express')
-const server = express()
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+
+  connectedUsers[user] = socket.id
+})
 
 // Variaveis
 const port = process.env.PORT || 3333
@@ -12,11 +23,18 @@ const mongo = 'mongodb+srv://EzequielGnich:26eg15111990eg@cluster0-ux1ju.mongodb
 const routes = require('./routes')
 
 // Configurações
-server.use(cors())
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
+
+  return next()
+})
+
+app.use(cors())
 mongoose.connect(mongo, {
   useNewUrlParser: true
 })
-server.use(express.json())
-server.use(routes)
+app.use(express.json())
+app.use(routes)
 
 server.listen(port, () => console.log(`Backend is running on port ${port}`))
